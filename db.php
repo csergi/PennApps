b1c61b7db5cca909bc01e446bcf130bd5dd59976
articDrag0n <picubed314@gmail.com>
2017-09-09 16:25:45 -0400

    asdf

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $dsn = getenv('MYSQL_DSN');
 $user = getenv('MYSQL_USER');
 $password = getenv('MYSQL_PASSWORD');
-$pdo = new PDO($dsn, $user, $password);
+$dbh = new PDO($dsn, $user, $password);
 $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $dbh->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
 ?>
