7fe42f7bd18ca153740ed2c450cd47998ad60502
root <root@ip-172-31-27-62.ec2.internal>
2017-09-10 04:05:09 +0000

    asdf

--- db.php
+++ db.php
@@ -1,12 +1,9 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$dbh = new PDO("mysql:host=$server;dbname=$db", $username, $password, $db);
+$host = 'localhost';
+$db = 'PennApps';
+$usr = 'dbuser';
+$pass = '007ShakenNotStirred%';
+$dbh = new PDO("mysql:host=$host;dbname=$db", $usr, $pass);
 $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES , false);
 $dbh->setAttribute(PDO::ATTR_STRINGIFY_FETCHES, false);
 ?>
