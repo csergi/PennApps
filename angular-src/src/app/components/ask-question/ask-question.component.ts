606e57a88d3f9d01a4722cdf70e8065b42384293
Zong Xu <zongx98@gmail.com>
2017-09-09 20:01:43 -0400

    Ask Question component fixed and updated

--- angular-src/src/app/components/ask-question/ask-question.component.ts
+++ angular-src/src/app/components/ask-question/ask-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { QuestionService } from '../../services/questionservice.service';
 
 @Component({
   selector: 'app-ask-question',
@@ -10,10 +11,33 @@ export class AskQuestionComponent implements OnInit {
   private warning : boolean = true;
   private questionTitle : string;
   private questionBody : string;
+  private tagsString : string;
+  private tagsArray = [];
+  private prevWord : string;
 
-  constructor() { }
+  constructor(private qService : QuestionService) { }
 
   ngOnInit() {
+  }
+
+  separateTags(event){
+    if(event.keyCode == 32){
+      if(this.prevWord){
+        this.tagsString.indexOf(this.prevWord);
+        this.tagsArray.push(this.tagsString);
+        this.prevWord = this.tagsArray[this.tagsArray.length - 1];
+      }
+    }
+    console.log(this.tagsArray);
+  }
+
+  askQuestion(){
+    console.log(this.tagsString);
+    var questionObj = {
+      "request" : "post",
+      "body" : this.questionBody,
+      "type" : 0
+    };
   }
 
 }
